be2b3b55e97592ab89be4eb3a5729fb9d0369f24
Teban18 <aepr_18_@hotmail.com>
2022-07-07 14:53:43 -0500

    optimized version 2

--- js/main.js
+++ js/main.js
@@ -1,104 +1,26 @@
-/* <a href="pokemon-info.html#id=0" target="_blank" class="pokemon-card">
-    <img src="https://i.ibb.co/hV3cj0k/Squirtle.png" alt="Squirtle">
-    <h4>Name 1</h4>
-    <p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</p>
-    <div class="number">1</div>
-</a> */
+var getData = (pokemonid) => {
+    fetch("https://pokeapi.co/api/v2/pokemon/"+pokemonid)  
+    .then(response => response.json())  
+    .then(function(pokeData) {  
+        setData(pokeData)  
+    })
+} 
 
+var setData = (object) => {
+    document.querySelector(".container-cards").innerHTML += `
+        <a href="pokemon-info.html#id=${object.id}" target="_blank" class="pokemon-card">
+        <img src="${object["sprites"]["other"]["dream_world"].front_default}" alt="${object.name}">
+        <h4>${object.name}</h4>
+        <div class="number">${object.id}</div>
+        </a> 
+    `;
+} 
 
-// Variables
-let start = 1
-let limitCards = 12
-
-// let test = "asdf".charAt(0).toUpperCase()
-// let test2 = "asdf".slice(1)
-// let fulltext = test+test2
-
-// let pokemonlist = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate" ]
-// let pokemonlist = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon" ]
-
-let pokemonSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/" // .svg
-
-let pokemonListURL = "https://pokeapi.co/api/v2/pokemon/" // ?offset=20&limit=20 // 1
-
-// calls
-let pokedex = document.querySelector(".container-cards")
-
-//
-
-// for(let i = 0; i < limitCards; i++){
-//     pokedex.innerHTML += `<a href="pokemon-info.html#id=0" target="_blank" class="pokemon-card">
-//     <img src="https://i.ibb.co/hV3cj0k/Squirtle.png" alt="Squirtle">
-//     <h4>Name 1</h4>
-//     <p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</p>
-//     <div class="number">1</div>
-// </a> `
-// }
-
-// for(let i = 0; i < limitCards; i++){
-//     if (pokemonlist[i]) {
-//         pokedex.innerHTML += `<a href="pokemon-info.html#id=0" target="_blank" class="pokemon-card">
-//         <img src="https://i.ibb.co/hV3cj0k/Squirtle.png" alt="Squirtle">
-//         <h4>${pokemonlist[i]}</h4>
-//         <p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</p>
-//         <div class="number">1</div>
-//         </a> `
-//     }
-// }
-
-// for(let i = start; i < limitCards+start; i++){
-//     if (pokemonlist[i-1]) {
-//         pokedex.innerHTML += `<a href="pokemon-info.html#id=${i}" target="_blank" class="pokemon-card">
-//         <img src="${pokemonSprite}${i}.svg" alt="${pokemonlist[i-1]}">
-//         <h4>${pokemonlist[i-1].charAt(0).toUpperCase()+pokemonlist[i-1].slice(1)}</h4>
-//         <p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</p>
-//         <div class="number">${i}</div>
-//         </a> `
-//     }
-// }
-
-
-let renderList = (list) => {
-    pokedex.innerHTML = ""
-    for(let i = 0; i < list.length; i++){
-        
-        let pokemonID = i + start
-        let pokemonName = list[i].name.charAt(0).toUpperCase()+list[i].name.slice(1)
-        // let pokemonName = list[i].name
-        
-        pokedex.innerHTML += `<a href="pokemon-info.html#id=${pokemonID}" target="_blank" class="pokemon-card">
-        <img src="${pokemonSprite}${pokemonID}.svg" alt="${pokemonName}">
-        <h4>${pokemonName}</h4>
-        <!--<p>Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</p>-->
-        <div class="number">${pokemonID}</div>
-        </a> `
+var counter = (num) => {
+    for (let i=1; i<num+1; i++) {
+        getData(i)
     }
 }
 
-async function logFetch(url) {
-    try {
-        const response = await fetch(url)
-        let pokemonInfo = await response.json()
-        return pokemonInfo
-    }
-    catch(e){
-        console.error(e)
-    }
-}
-
-
-async function mainFuntion() {
-    try{
-        // let pokemonList = await logFetch(pokemonListURL+`?offset=${start-1}&limit=${limitCards}`)
-        let pokemonList = await logFetch("../json/list0-20.json")
-        renderList(pokemonList.results)
-    } catch(e) {
-        console.error(e)
-    }
-}
-
-
-mainFuntion()
-
-
+counter(100);
 
--- js/p-info.js
+++ js/p-info.js
@@ -1,75 +1,28 @@
-// Variables
+var getDataDesc = (pokemonid) => {
+    fetch("https://pokeapi.co/api/v2/pokemon/"+pokemonid)  
+    .then(response => response.json())  
+    .then(function(pokeData) {  
+        console.log(pokeData["abilities"]);
+        setDataDesc(pokeData);
+    })
+} 
 
-let siteURL = window.location.toString()
-//"http://127.0.0.1:5500/pokemon-info.html#id=20"
-let vars = siteURL.split("=")
-// [
-//     "http://127.0.0.1:5500/pokemon-info.html#id",
-//     "20"
-// ]
-let pokemonID = vars[1]
-
-let pokemonListURL = "https://pokeapi.co/api/v2/pokemon/"
-
-
-// Calls
-let pokemonImage = document.querySelector(".pokemon-image img")
-let typeSection = document.querySelector("#tipo")
-let height = document.querySelector("#altura")
-let width = document.querySelector("#peso")
-let nameSection = document.querySelector(".pokemon-name p")
-let abilitiesSection = document.querySelector("#pokemon-abilities")
-
-// Functions 
-async function logFetch(url) {
-    try {    
-        const res = await fetch(url)
-        return await res.json()
-    }
-    catch(e){
-        console.error(e)
-    }
+var getId = (spliter) => {
+    getDataDesc(window.location.toString().split(spliter)[1]);
 }
 
-let renderInfo = (pokeInfo) => {
-    let sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg`
-    let pokemonName = pokeInfo.name
-    let altura = pokeInfo.height
-    let peso = pokeInfo.weight
+getId("=")
 
-    let tipo = []
-    for (let type of pokeInfo.types) {
-        tipo.push(" "+type.type.name)
-    }
+var setDataDesc = (object) => {
+    document.querySelector(".pokemon-image img").setAttribute("src", object["sprites"]["other"]["dream_world"].front_default);
+    document.querySelector("#tipo").innerHTML = iterator(object["types"]).type.name;
+    document.querySelector("#altura").innerHTML = object["height"] + "m";
+    document.querySelector("#peso").innerHTML = object["weight"] + "kg";
+    document.querySelector(".pokemon-name p").innerHTML = object["name"];
+    document.querySelector("#pokemon-abilities").innerHTML = iterator(object["abilities"]).ability.name;
+} 
 
-    let abilities = []
-    for (let ability of pokeInfo.abilities) {
-        abilities.push(ability.ability.name)
-    }
-    console.log(abilities)
-    
-    pokemonImage.setAttribute("src", sprite)
-    pokemonImage.setAttribute("alt", pokemonName)
-
-    typeSection.innerHTML = tipo.toString()
-    height.innerHTML = `${altura} m`
-    width.innerHTML = `${peso} kg`
-
-    nameSection.innerHTML = pokemonName
-
-    for(let ability of abilities){
-        abilitiesSection.innerHTML = `<li>${ability}</li>`
-    }
-    
+var iterator = (array) => {
+    for (let e of array)
+        return e
 }
-
-async function mainFuntion() {
-    try{
-        let pokemonInfo = await logFetch(pokemonListURL+`${pokemonID}`)
-        renderInfo(pokemonInfo)
-    } catch(e) {
-        console.error(e)
-    }
-}
-
-mainFuntion()
